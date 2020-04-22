cca6f38021d1fe571b23c196a862e1192065bbde
Josh Ghiloni <jghiloni@pivotal.io>
2020-04-21 21:30:32 -0600

    initial commit

---
+++ init.sh
@@ -0,0 +1,14 @@
+
+alias git='hub'
+
+alias reload='source ~/.zshrc'
+
+alias fuck='sudo $(fc -ln -1)'
+
+alias gs='git status'
+
+alias wtf='git diff'
+
+alias syncfork='function { git fetch $1; git merge $1/master; }'
+
+alias brewup='brew doctor && brew update && brew upgrade && brew cleanup && brew linkapps'
